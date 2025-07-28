66d712565c04bc70f5ab9ed4915fdedda0efe6e8
artyomkorchagin <steamartem@gmail.com>
2025-07-28 20:03:37 +0300

    Implemented connection to postgres and passing services into bot

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/artyomkorchagin/storeyourimages/internal/services/content"
+	"github.com/artyomkorchagin/storeyourimages/internal/services/users"
+	pgxcontent "github.com/artyomkorchagin/storeyourimages/internal/storage/pgx/content"
+	pgxusers "github.com/artyomkorchagin/storeyourimages/internal/storage/pgx/users"
+	bot "github.com/artyomkorchagin/storeyourimages/internal/tg-bot"
+	"github.com/artyomkorchagin/storeyourimages/pkg/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := sql.Open("pgx", config.GetDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	repoContent := pgxcontent.NewRepository(db)
+	svcContent := content.NewService(repoContent)
+
+	repoUsers := pgxusers.NewRepository(db)
+	svcUsers := users.NewService(repoUsers)
+
+	svcs := bot.NewAllServices(svcUsers, svcContent)
+	log.Println("Connected to database")
+	bot.InitBot(svcs)
 }
